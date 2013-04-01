215a12081a5cdec071b4cdb756669464ff9213e7
Wes Morgan <cap10morgan@gmail.com>
2013-04-01 16:00:02 -0600

    commit latest personal settings changes

--- personal/wmorgan.el
+++ personal/wmorgan.el
@@ -1,4 +1,4 @@
-; Use Solarized dark theme
+;; Use Solarized dark theme
 (load-theme 'solarized-dark t)
 
 ;; Start server
@@ -13,20 +13,24 @@
                       (cons (concat (getenv "HOME") "/.rbenv/bin")
                             exec-path)))
 
-; Turn on evil mode (Vim navigation FTW)
+;; Scrolling
+(setq mouse-wheel-scroll-amount '(1))
+(setq mouse-wheel-progressive-speed nil)
+
+;; Turn on evil mode (Vim navigation FTW)
 (evil-mode 1)
 
-; Set evil shift-width to 2
+;; Set evil shift-width to 2
 (setq evil-shift-width 2)
 
-; Turn on enclose-mode globally
-(enclose-global-mode)
+;; Turn on autopair-mode globally
+(autopair-global-mode t)
 
 ;; Map Command-/ to comment-or-uncomment-region
 (define-key global-map (kbd "s-/") 'comment-or-uncomment-region)
 (define-key global-map (kbd "C-c /") 'comment-or-uncomment-region)
 
-; Make comment toggles operate on lines when no region is active
+;; Make comment toggles operate on lines when no region is active
 (defadvice comment-or-uncomment-region (before slick-comment activate compile)
   "When called interactively with no active region, comment a single line instead."
   (interactive
@@ -34,20 +38,20 @@
      (list (line-beginning-position)
            (line-beginning-position 2)))))
 
-; Better auto-indenting
+;; Better auto-indenting
 (define-key global-map (kbd "RET") 'comment-indent-new-line)
 (electric-indent-mode)
 
-; Use Source Code Pro font
+;; Use Source Code Pro font
 (set-face-attribute 'default nil :font "Source Code Pro-13")
 
-; Always show line numbers
+;; Always show line numbers
 (global-linum-mode 1)
 
-; Set JavaScript indent level to 2
+;; Set JavaScript indent level to 2
 (setq js-indent-level 2)
 
-; Activate powerline
+;; Activate powerline
 (add-to-list 'load-path "~/.emacs.d/vendor/emacs-powerline")
 (require 'powerline)
 (setq powerline-color1 "#657b83")
@@ -59,5 +63,11 @@
 (set-face-attribute 'mode-line-inactive nil
                     :box nil)
 
-; Ruby
+;; Ruby
 (setq ruby-end-insert-newline nil)
+
+;; Git gutter
+(require 'git-gutter-fringe)
+(setq git-gutter-fr:side 'right-fringe)
+(setq-default right-fringe-width 15)
+(global-git-gutter-mode t)
